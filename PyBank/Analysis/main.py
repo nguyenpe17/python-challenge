9949b8442183fb60df50ab27978524dcc86174d9
Peter Nguyen <nguyen.peter24@gmail.com>
2020-04-02 11:43:08 -0700

    Update main.py

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -5,23 +5,23 @@ with open(file,'r') as csvfile:
 
 # Set Variables
 total_months = 0
-total_proift/loss = 0
+total_proift_loss = 0
 
-prev_profit/loss = 0
-profit/loss_change = 0
+prev_profit_loss = 0
+profit_loss_change = 0
 greatest_increase = ["", 0]
 greatest_decrease = ["", 9999999]
 
-profit/loss_change = []
+profit_loss_change = []
 
     # Lopp through all the rows of data collected
-    for row in reader:
+for row in reader:
 
         #Calculate the totals
         total_months = total_months + 1
-        total_proift/loss = total_proift/loss + int(row["Profit/Losses"])
-        #print(row)
+        total_proift_loss = total_proift_loss + int(row["Profit/Losses"])
+        print(row)
 
         # Keep track of changes
-        profit/loss_change = int(row["Profit/Losses"]) - prev_profit/loss
-        # print(profit/loss_change)
+        profit_loss_change = int(row["Profit/Losses"]) - prev_profit_loss
+        print(profit_loss_change)
